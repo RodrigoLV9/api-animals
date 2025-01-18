ad097e15c31a56fa21307525d4794e4b008239e0
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-01-18 15:03:19 -0300

    feat(server): Add new router of animals.js and add/modify paths

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ require('dotenv').config()
 const cors=require('cors')
 const PORT = process.env.PORT || 5000;
 const data1=require('./data/data1.json')
+const animalsRouter=require('./routers/animals')
 
 server.use(cors())
-server.get(('/'),(req,res)=>{
+server.get('/animals',(req,res)=>{
     res.send(data1)
 })
-
+server.get('/animals/vertebrates',(req,res)=>{
+    res.send(data1.vertebrados)
+})
+server.use('/animals/vertebrates/',animalsRouter)
 server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
