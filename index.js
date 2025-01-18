c1a1459e6e0396328939b80c91caf43e2ab6e5d6
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-01-17 23:56:16 -0300

    feat(dependencies): Install cors and dotenv and use them in index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express=require('express');
 const server=express();
-const PORT=5000;
+require('dotenv').config()
+const cors=require('cors')
+const PORT = process.env.PORT || 5000;
+const data1=require('./data/data1.json')
 
+server.use(cors())
 server.get(('/'),(req,res)=>{
-    res.send('Hello World')
+    res.send(data1)
 })
 
 server.listen(PORT,()=>{
-    console.log(`Server in ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
 
