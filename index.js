5a5856bef5f48db30abeb78cb271f80970332a94
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-01-25 23:23:24 -0300

    feat(index.js): Split the code structure

--- index.js
+++ index.js
@@ -1,53 +1,30 @@
-const express=require('express');
-const server=express();
-require('dotenv').config()
-const cors=require('cors')
+const express = require('express');
+const server = express();
+require('dotenv').config();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
-const dataEN=require('./data/dataEN.json')
-/* const dataES=require('./data/dataES.json') */
-const animalsRouter=require('./routers/animals')
-const handleParams=(str)=>{
-    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-}
-server.use(cors())
-server.get('/animals',(req,res)=>{
-    const {animal}=req.query
-    if(animal){
-        const animalString=handleParams(animal)
-        const saveAnimal=dataEN.vertebrates.map((item)=>{
-            return item.examples.find((e)=>{
-                return handleParams(e.common_name)==animalString
-            })
-        })
-        const saveAnimal2=dataEN.invertebrates.map((item)=>{
-            return item.examples.find((e)=>{
-                return handleParams(e.common_name)==animalString
-            })
-        })
-        const result=[...saveAnimal, ...saveAnimal2].filter((el) => el != null)
-        
-        if (result.length === 0) {
-            console.log("hello")
-            res.status(404).send('<h1>Error 404 (Not Found)</h1>');
-        } else {
-            res.send(result);
-        }
-    }else{
-        res.send(dataEN)
-    }
+const dataEN = require('./data/dataEN.json');
+const dataES = require('./data/dataES.json');
+const langMiddleware = require('./middlewares/language.js');
+const animalsRouter = require('./routers/generalAnimals.js');
+const vertebratesRouter=require('./routers/vertebrates')
+const invertebratesRouter=require('./routers/invertebrates')
+server.use(cors());
+server.use(langMiddleware);
+server.get('/',(req,res)=>{
+    res.status(200).send('<h1>Welcome to the Animals API</h1>')
 })
-server.get('/animals/vertebrates',(req,res)=>{
-    res.send(dataEN.vertebrates)
-})
-server.use('/animals/vertebrates/',animalsRouter)
-server.get('/animals/invertebrates',(req,res)=>{
-    res.send(dataEN.invertebrates)
-})
-server.use((req,res)=>{
-    res.status(404).send('<h1>Error 404 (Not Found)</h1>')
-})
-server.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
-
-
+server.use('/animals',animalsRouter)
+server.use('/animals/vertebrates',vertebratesRouter)
+server.use('/animals/invertebrates',invertebratesRouter)
+server.use((req, res) => {
+    res.status(404).send('<h1>Error 404 (Not Found)</h1>');
+});
+// Middleware para manejar errores
+server.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal');
+});
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
